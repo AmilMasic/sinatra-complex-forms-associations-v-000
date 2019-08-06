c5e918e7b779a9ef593e0dc81e03c39be3b9b9e5
Amil Masic <amil.masic@gmail.com>
2019-08-06 20:01:18 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -20,7 +20,7 @@ class PetsController < ApplicationController
 
   get '/pets/:id/edit' do
     @pet = Pet.find(params[:id])
-    @owner = Owner.all
+    # @owner = Owner.all
     erb :'/pets/edit'
   end
 
