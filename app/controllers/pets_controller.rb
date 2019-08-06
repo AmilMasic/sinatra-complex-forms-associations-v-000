e2ac12504b544c6e590cdd988ae61b99e1a96314
Amil Masic <amil.masic@gmail.com>
2019-08-06 19:55:26 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -32,6 +32,7 @@ class PetsController < ApplicationController
   patch '/pets/:id' do
     # binding.pry
     @pet = Pet.find(params[:id])
+    @pet.update(params[:pet])
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
     end
